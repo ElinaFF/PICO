ccbd9406d329d172d8e285328bb1a987776bca5b
le-smog <le_smog@gmx.com>
2022-01-24 16:40:31 -0500

    main file

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from dash import html
+from dash.dependencies import Input, Output
+import dash
+import dash_bootstrap_components as dbc
+
+from tabs import *
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX], meta_tags=[{"name": "viewport", "content": "width=device-width"}])
+server = app.server
+# app.scripts.config.serve_locally = False
+app.css.config.serve_locally = False
+app.config.suppress_callback_exceptions = True
+app.css.append_css({
+    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
+
+
+app.layout = html.Div(id="page", children=[
+        html.Div(id="dataCache", children=[
+
+        ],
+        style={"display": "none"}
+        ),
+        html.Div(id="title_container", className="row", children=[
+            html.Div(html.H1(id="title", children="MetaboDashboard"), id="title_bg")  #, style={"text-align": "center"}
+        ]),
+        html.Div(id="main-content", children=[
+            dbc.Tabs(id="custom_big_tabs",
+                     className="global_tabs_container",
+                     children=[infoLayout, splitsLayout, MLLayout, resultsLayout, interpretLayout])
+        ]),
+    ])
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8080)
