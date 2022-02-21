b448d03a5d53bc75349da941823a792e5f01716b
le-smog <le_smog@gmx.com>
2022-02-21 15:53:42 -0500

    bug: setup.py OK

--- metabodashboard/setup.py
+++ metabodashboard/setup.py
@@ -9,18 +9,19 @@ setup(
     # url='https://blog.godatadriven.com/setup-py',
     packages=find_packages(include=['metabodashboard', 'metabodashboard.*']),
     install_requires=[
-        'dash==2.1.0',
-        'dash-bio==0.9.0',
-        'dash-bootstrap-components==1.0.2',
-        'dash-core-components==2.0.0',
-        'dash-html-components==2.0.0',
-        'dash-renderer==1.0.0',
-        'dash-table==5.0.0',
-        'numpy==1.20.3',
-        'pandas==1.2.5',
-        'plotly==5.5.0',
-        'fastcluster==1.2.4',
-        'pyMSpec @ git+https://github.com/alexandrovteam/pyMSpec@v0.1',
-        'msvlm @ git+https://github.com/francisbrochu/msvlm'
+        'scipy==1.2.1',
+        'dash',
+        'dash-bio',
+        'dash-bootstrap-components',
+        'dash-core-components',
+        'dash-html-components',
+        'dash-renderer',
+        'dash-table',
+        'numpy>=1.16.5',
+        'pandas',
+        'plotly',
+        'scikit-learn',
+        # 'pyMSpec @ git+https://github.com/alexandrovteam/pyMSpec',
+        # 'msvlm @ git+https://github.com/francisbrochu/msvlm'
         ]
 )
