0230dc258f3c05f39f8666dd42671fd889e37c18
le-smog <le_smog@gmx.com>
2022-02-28 11:35:08 -0500

    feat: direct pip from git

--- launcher.py
+++ launcher.py
@@ -3,7 +3,7 @@ import os
 try:
     from metabodashboard import app
 except ImportError:
-    os.system("pip install .")
+    os.system("pip install git+https://github.com/ElinaFF/MetaboDashboard.git")
     from metabodashboard import app
 
 if __name__ == "__main__":
