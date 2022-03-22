35bfa36dc5afc5a7fdd72c752fef73037f0daee0
le-smog <le_smog@gmx.com>
2022-03-22 11:25:33 -0400

    fix: split group now own classes_design (needed for target pairing)

--- metabodashboard/domain/SplitGroup.py
+++ metabodashboard/domain/SplitGroup.py
@@ -14,21 +14,22 @@ class SplitGroup:
         self._metadata = metadata
         self._template_file_name = experiment_name + "_split_{}.p"
         self._number_of_split = number_of_splits
-        self._computeSplits(train_test_proportion, number_of_splits, classes_design)
+        self._classes_design = classes_design
+        self._compute_splits(train_test_proportion, number_of_splits)
 
-    def _computeSplits(self, train_test_proportion: float, number_of_splits: int, classes_design: dict):
+    def _compute_splits(self, train_test_proportion: float, number_of_splits: int):
         for split_index in range(number_of_splits):
-            X_train, X_test, y_train, y_test = train_test_split(self._metadata.loadSamplesId(),
-                                                                self._metadata.loadTargets(),
+            X_train, X_test, y_train, y_test = train_test_split(self._metadata.load_samples_id(),
+                                                                self._metadata.load_classes(),
                                                                 test_size=train_test_proportion,
                                                                 random_state=split_index)
 
             with open(os.path.join(DUMP_PATH, self._template_file_name.format(split_index)), "w+b") as split_file:
                 pickle.dump([X_train, X_test, y_train, y_test], split_file)
 
-    def loadSplitWithIndex(self, split_index: int) -> list:
+    def load_split_with_index(self, split_index: int) -> list:
         with open(os.path.join(DUMP_PATH, self._template_file_name.format(split_index)), "rb") as split_file:
             return pickle.load(split_file)
 
-    def getNumberOfSplits(self):
+    def get_number_of_splits(self):
         return self._number_of_split
