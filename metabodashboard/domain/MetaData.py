ea105f276ff9f74d3a2da6749229cd976ba0b7b3
le-smog <le_smog@gmx.com>
2022-03-22 11:23:50 -0400

    fix: get classes instead of targets (setted with classes design)

--- metabodashboard/domain/MetaData.py
+++ metabodashboard/domain/MetaData.py
@@ -7,8 +7,8 @@ ROOT_PATH = os.path.dirname(__file__)
 DUMP_PATH = os.path.join(ROOT_PATH, os.path.join("dumps", "metadata"))
 DUMP_METADATA_PATH = os.path.join(DUMP_PATH, "metadata.p")
 DUMP_METADATA_COLUMNS_PATH = os.path.join(DUMP_PATH, "metadata_columns.p")
-DUMP_TARGETS_PATH = os.path.join(DUMP_PATH, "targets.p")
 DUMP_SAMPLES_ID_PATH = os.path.join(DUMP_PATH, "samples_id.p")
+DUMP_CLASSES_PATH = os.path.join(DUMP_PATH, "classes.p")
 
 
 class MetaData:
@@ -21,48 +21,54 @@ class MetaData:
 
         self._id_column = None
         self._target_column = None
+        self._classes_design = None
 
-    def loadMetadata(self) -> pd.DataFrame:
+    def load_metadata(self) -> pd.DataFrame:
         with open(DUMP_METADATA_PATH, "rb") as metadata_file:
             return pickle.load(metadata_file)
 
-    def loadColumns(self):
+    def load_columns(self):
         with open(DUMP_METADATA_COLUMNS_PATH, "rb") as metadata_file:
             return pickle.load(metadata_file)
 
-    def getFormattedColumns(self) -> list:
-        columns_with_none = ["None"] + self.loadColumns()
+    def get_formatted_columns(self) -> list:
+        columns_with_none = ["None"] + self.load_columns()
         return [{'label': column, 'value': column} for column in columns_with_none]
 
-    def getFormattedUniqueTargets(self) -> list:
-        targets = self.loadTargets()
+    def get_formatted_unique_targets(self) -> list:
+        targets = self.load_classes()
         possible_targets = list(set(targets))
         return [{'label': target, 'value': target} for target in possible_targets]
 
-    def setIdColumn(self, id_column: str):
-        df_metadata = self.loadMetadata()
+    def set_id_column(self, id_column: str):
+        df_metadata = self.load_metadata()
         with open(DUMP_SAMPLES_ID_PATH, "w+b") as metadata_file:
             pickle.dump(df_metadata[id_column].tolist(), metadata_file)
 
         self._id_column = id_column
 
-    def setTargetColumn(self, target_column: str):
-        df_metadata = self.loadMetadata()
-        with open(DUMP_TARGETS_PATH, "w+b") as metadata_file:
-            pickle.dump(df_metadata[target_column].tolist(), metadata_file)
-
+    def set_target_column(self, target_column: str):
         self._target_column = target_column
 
-    def loadSamplesId(self) -> list:
+    def set_classes_design(self):
+        targets = self.load_classes()
+        reverse_classes_design = {target: class_ for class_, target in self._classes_design.items()}
+        classes = []
+        for target in targets:
+            classes.append(reverse_classes_design[target])
+        with open(DUMP_CLASSES_PATH, "w+b") as classes_file:
+            pickle.dump(classes, classes_file)
+
+    def load_samples_id(self) -> list:
         if self._id_column is None:
             raise RuntimeError("Try to access the samples id column before setting one")
         with open(DUMP_SAMPLES_ID_PATH, "rb") as metadata_file:
             return pickle.load(metadata_file)
 
-    def loadTargets(self) -> list:
-        if self._target_column is None:
-            raise RuntimeError("Try to access the target column before setting one")
-        with open(DUMP_TARGETS_PATH, "rb") as metadata_file:
+    def load_classes(self) -> list:
+        if self._classes_design is None:
+            raise RuntimeError("Try to access the classes before setting the column.")
+        with open(DUMP_CLASSES_PATH, "rb") as metadata_file:
             return pickle.load(metadata_file)
 
-# TODO: getGroupByDesignTarget(self, classes_design: dict):
+# TODO: join sampleId and target in same pickle file
