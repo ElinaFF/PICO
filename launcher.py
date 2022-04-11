73484c8453b55db868adb7945a303b7058932b2f
le-smog <le_smog@gmx.com>
2022-04-11 12:05:28 -0400

    test: change MetaboDashboard to app (instead of package)

--- launcher.py
+++ launcher.py
@@ -1,11 +1,32 @@
 import os
 
-try:
-    from metabodashboard import app
-except ImportError:
-    os.system("pip install git+https://github.com/ElinaFF/MetaboDashboard.git@refactoring")
-    from metabodashboard import app
+import pkg_resources
+
+with open('requirement.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
 
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8080, host='0.0.0.0')
+    print("MetaboDashboard is starting...")
+    print()
+    print("Checking dependencies...")
+    try:
+        pkg_resources.require(requirements)
+    except pkg_resources.DistributionNotFound:
+        print("Dependencies not found. Installation is starting...")
+        print()
+        os.system("pip install -r requirement.txt")
+        try:
+            pkg_resources.require(requirements)
+        except pkg_resources.DistributionNotFound as error:
+            print()
+            print("Installation failed with error:", str(error))
+            print("Please install MetaboDashboard dependencies manually.")
+            exit()
 
+    print("Dependencies are OK.")
+    print()
+
+    from metabodashboard import app
+
+    app.run_server(debug=True, port=8080, host='localhost')
