502d05a6383502b11022c581815d4b47682248c6
VincentVilain <74467496+VincentVilain@users.noreply.github.com>
2022-05-09 17:00:21 -0400

    setup conda environment and rebuild launcher

--- launcher.py
+++ launcher.py
@@ -5,59 +5,118 @@ import subprocess
 #import git
 
 REQUIREMENT_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), 'requirement.txt'))
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+MINICONDA_EXEFILE = os.path.abspath(os.path.join(ROOT_DIR, 'Miniconda3-latest.exe'))
 
 def checkIfMinimumFileRequirementExist():
     print("Checking minimum file requirement...")
     return os.path.exists('matabodashboard') and os.path.exists('requirement.txt')
 
+
 def installFromGitHub():
     print("installing from git \n")
 
 
-def installDepencies():
-    print("Installing dependencies...")
-    return True
-    with open(REQUIREMENT_FILE, 'r') as f:
-        requirements = f.read().splitlines()
-    print("Dependencies not found. Installation is starting...\n")
-    os.system("python -m pip install --user -r " + REQUIREMENT_FILE)
-    try:
-        pkg_resources.require(requirements)
-    except (pkg_resources.DistributionNotFound, pkg_resources.VersionConflict) as error:
-        print("\nInstallation failed with error:", str(error))
-        print("Please install MetaboDashboard dependencies manually.")
-        exit()
+def downloadMiniconda(url_for_download: str):
+    request = requests.get(url_for_download)
+    with open(MINICONDA_EXEFILE, 'wb') as exeFile:
+        exeFile.write(request.content)
 
-def checkForDepencies():
-    with open(REQUIREMENT_FILE, 'r') as f:
-        requirements = f.read().splitlines()
+
+def installMinicondaForWindows():
+    conda_for_windows = f"https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86{'_64' if is_os_64bit() else ''}.exe"
+    downloadMiniconda(conda_for_windows)
+    install_conda_command = f"start /wait {MINICONDA_EXEFILE} /InstallationType=JustMe /RegisterPython=0 /S " \
+                            f"/AddToPath=1 /D=%UserProfile%\Miniconda3"
+    subprocess.check_call(install_conda_command, shell=True)
+    subprocess.check_call("set PATH=%PATH%;%UserProfile%\\Miniconda3\\Library\\bin", shell=True)
+
+
+def installMinicondaForLinux():
+    conda_for_linux = f"https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86{'_64' if is_os_64bit() else ''}.sh"
+    downloadMiniconda(conda_for_linux)
+    install_conda_command = f"bash {MINICONDA_EXEFILE}"
+    subprocess.check_call(install_conda_command, shell=True)
+
+
+def installMinicondaForMacOS():
+    conda_for_macos = f"https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86{'_64' if is_os_64bit() else ''}.sh"
+    downloadMiniconda(conda_for_macos)
+    install_conda_command = f"bash {MINICONDA_EXEFILE}"
+    subprocess.check_call(install_conda_command, shell=True)
+
+
+def is_conda_installed() -> bool:
     try:
-        pkg_resources.require(requirements)
-        return True
-    except (pkg_resources.DistributionNotFound, pkg_resources.VersionConflict) as error:
-        print(str(error))
+        subprocess.check_call("conda env list", shell=True)
+    except subprocess.CalledProcessError:
         return False
+    return True
+
+
+def is_metabodashboard_env_exist() -> bool:
+    enviList = subprocess.check_output("conda env list", shell=True)
+    if "metabodashboard" in enviList.decode('utf-8'):
+        return True
+    return False
+
+
+def create_metabodashboard_env():
+    subprocess.check_call("conda create -n metabodashboard -f environment.yml", shell=True)
+
+
+def is_os_64bit():
+    return platform.machine().endswith('64')
+
+
+# TODO : add version verification (useless at first sight)
+def env_dependencies_verification():
+    # Contient OBLIGATOIREMENT un '=={version}'
+    actualPackageInstalledList = subprocess.check_output("pip freeze")
+    with open(REQUIREMENT_FILE, 'r') as f:
+        line = f.readline()
+        while line:
+            lineWithoutVersion = line.split('==')[0]
+            if lineWithoutVersion not in actualPackageInstalledList.decode('utf-8'):
+                print(f"{lineWithoutVersion} couldn't be installed")
+                return False
+    return True
+
 
 def main():
     osUsed = platform.system()
-    versionPython = platform.python_version()
 
+    print("Checking for conda installation...")
+    if not is_conda_installed():
+        print("conda not found !")
+        print("Installing MiniConda...")
+        if osUsed == "Windows":
+            installMinicondaForWindows()
+        elif osUsed == "Linux":
+            installMinicondaForLinux()
+        elif osUsed == "Darwin":
+            installMinicondaForMacOS()
+        print("Conda installation done.\nPlease restart the launcher")
+        exit(0)
+
+    print("\nChecking for metabodashboard environment...")
+    if not is_metabodashboard_env_exist():
+        print("metabodashboard environment not found !")
+        print("Creating metabodashboard environment...")
+        create_metabodashboard_env()
+
+    print("\nChecking for source code...")
     if not checkIfMinimumFileRequirementExist():
-        # TODO : impossible de run avec la commande python3
-        print("\033[33m{}\033[00m".format("WARNING : This script is not compatible with the command python3\n"))
-        print("MetaboDashboard is starting... \n")
+        print("Source code not found !")
+        print("Downloading source code...")
+        installFromGitHub()
 
-    print("Checking dependencies...")
-    if not checkForDepencies():
-        installDepencies()
+    subprocess.check_call("conda activate metabodashboard")
 
-    if checkIfMinimumFileRequirementExist():
-        print("Dependencies are OK.\n")
-        print("Server is starting...\n")
-        from metabodashboard import app
-        app.run_server(debug=True, host='127.0.0.1', port=5000)
-    else :
-        print("Installation Error : Please check the metadashboard folder.")  # TODO : ajouter un message d'erreur
+    if not env_dependencies_verification():
+        raise RuntimeError("Missing package in the conda environment after setup\nTry installing it manually")
+
+    print("Success !")
 
 
 if __name__ == "__main__":
