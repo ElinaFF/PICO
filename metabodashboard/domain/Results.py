011ad974ad4a8e92d3083c569584b08119e2f36f
ElinaFF <elina.francovic-fontaine.1@ulaval.ca>
2022-12-07 13:33:33 -0500

    updated Results, plots, results tab

--- metabodashboard/domain/Results.py
+++ metabodashboard/domain/Results.py
@@ -262,11 +262,11 @@ class Results:
         nbr_train = len(y_train_true)
         nbr_test = len(y_test_true)
         tot = nbr_train + nbr_test
+        ratio_test = round(nbr_test / tot * 100)
+        ratio_train = 100 - ratio_test
         nom_stats = ["Number of samples", "Train-test repartition"]
         valeurs_stats = [str(tot)]
-        valeurs_stats.append(
-            str(math.ceil(nbr_train / tot * 100)) + " - " + str(math.ceil(nbr_test / tot * 100))
-        )
+        valeurs_stats.append(str(ratio_train) + "% - " + str(ratio_test) + "%")
         y = y_train_true + y_test_true
         c = Counter(y)
         for k in c.keys():
