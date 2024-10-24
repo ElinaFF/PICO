6bed80482e27a3b769f25f623a729502d6cce781
Élina Francovic-Fontaine <elina.francovic-fontaine.1@ulaval.ca>
2024-10-24 12:39:06 -0400

    started changing params
    #133

--- medic/conf/SupportedModels.py
+++ medic/conf/SupportedModels.py
@@ -51,8 +51,8 @@ LEARN_CONFIG_RS = {
     "DecisionTree": {
         "function": DecisionTreeClassifier,
         "ParamGrid": {
-            "max_depth": np.linspace(1, 10, dtype=int),
-            "min_samples_split": np.linspace(0.01, 1, 10),
+            "max_depth": np.arange(1, 7, step=1, dtype=int), #[1, 2, 3, 4, 5, 6],
+            "min_samples_split": np.arange(2, 20, step=2, dtype=int), #[2, 4, 6, 8, 10],
             "max_features": ["sqrt", "log2"],
         },
         "importance_attribute": "feature_importances_",
@@ -60,11 +60,9 @@ LEARN_CONFIG_RS = {
     "RandomForest": {
         "function": RandomForestClassifier,
         "ParamGrid": {
-            "n_estimators": np.linspace(20, 80, dtype=int),
-            "max_depth": np.linspace(1, 5, dtype=int),
-            "min_samples_split": np.linspace(0.01, 1, 10),
-            "max_features": ["sqrt", "log2"],
-            "max_samples": np.linspace(0.2, 1, 10),
+            "n_estimators": np.arange(5, 400, step=10, dtype=int), #[5, 10, 30, 70, 100, 200, 500],
+            "max_depth": np.arange(1, 6, step=1, dtype=int), #[1, 2, 3, 4, 5],
+            "min_samples_split": np.arange(2, 20, step=2, dtype=int), #[2, 4, 6, 8, 10],
         },
         "importance_attribute": "feature_importances_",
     },
@@ -81,10 +79,8 @@ LEARN_CONFIG_RS = {
         "function": RandomScmClassifier,
         "ParamGrid": {
             "p": np.linspace(0.01, 3, 10),
-            "n_estimators": np.linspace(20, 80, 10, dtype=int),
+            "n_estimators": np.arange(5, 200, step=10, dtype=int)
             "model_type": ["conjunction", "disjunction"],
-            "max_features": ["auto", "sqrt", "log2"],
-            "max_samples": np.linspace(0.2, 1, 10),
         },
         "importance_attribute": "feature_importances_",
     },
