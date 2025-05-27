8e80142c89aaf73dc03f4999cdbffd5d62a27778
ElinaFF <elina.francovic-fontaine.1@ulaval.ca>
2025-05-27 14:55:34 -0400

    Deeply cleaned automate (functions, comments)

--- automate.py
+++ automate.py
@@ -4,13 +4,26 @@ import numpy as np
 import pandas as pd
 import pickle as pkl
 import umap
-
 from medic.domain import MetaboController
 from medic.service import Utils
 import dash_bootstrap_components as dbc
 import plotly.express as px
 from collections import Counter
 
+##############################
+#        Informations        #
+##############################
+
+# This file has been divided in functions in an attempt to make it more digestible/clear
+# It tries to resemble the UI version, in the order and division of tasks
+# There is parameters to adjust in the main function,
+# But most of the customization happens in the body of smaller functions, 
+# so be carefull not to forget anything.
+
+
+##############################
+#   Define paths variables   #
+##############################
 
 METADATA_PATH = "../medic_otherThanPackage/s_MTBLS28_merged.csv"
 DATAMATRIX_PATH = "../medic_otherThanPackage/MTBLS28_CombinedData2_forML.csv"
@@ -22,57 +35,105 @@ DATAMATRIX_PATH = "../medic_otherThanPackage/MTBLS28_CombinedData2_forML.csv"
 #DATAMATRIX_PATH = "../medic_otherThanPackage/MTBLS28_data_NEG_forML.csv"
 
 
+##############################
+#      Setup experiment      #
+##############################
+
+def SPLITS_setup_files(mtb_ctrl):
+    """
+    mtb_ctrl : MetaboController object
+    """
+    mtb_ctrl.set_raw_use_for_data(False)
+    mtb_ctrl.set_data_matrix_remove_rt(False)
+    mtb_ctrl.set_data_matrix_from_path(DATAMATRIX_PATH, from_base64=False)
+    mtb_ctrl.set_metadata(METADATA_PATH, from_base64=False)
+
+def SPLITS_setup_classification_designs(mtb_ctrl):
+    """
+    mtb_ctrl : MetaboController object
+    """
+    mtb_ctrl.set_id_column("Sample_Name")
+    mtb_ctrl.set_target_columns(["Factor Value[Sample Type]"])
+    #mtb_ctrl.set_target_columns(["Factor Value[Sample Type]", "Factor Value[Smoking]"])
+
+    # Designs are defined as dict with labels as keys and column names(if multiple link with "__") as values
+    mtb_ctrl.add_experimental_design({"Cases": ["Case"], "Ctrls": ["Control"]})
+    #mtb_ctrl.add_experimental_design({"NEG_Case_Current": ["Case__Current Smoker"], "Ctrl_Current": ["Control__Current Smoker"]})
+    #mtb_ctrl.add_experimental_design({"NEG_Case_Former": ["Case__Former Smoker"], "Ctrl_Former": ["Control__Former Smoker"]})
+    #mtb_ctrl.add_experimental_design({"NEG_Case_Never": ["Case__Never Smoker"], "Ctrl_Never": ["Control__Never Smoker"]})
+
+
+def SPLITS_setup_splits_and_balancing(mtb_ctrl, proportion_splits , nbr_splits):
+    """
+    mtb_ctrl : MetaboController object
+    proportion_splits : proportion of sample in test set in decimal (0 to 1)
+    nbr_splits : number of splits to compute
+    """
+    mtb_ctrl.set_train_test_proportion(proportion_splits)
+    mtb_ctrl.set_number_of_splits(nbr_splits)
+
+    # The balance correction is specific to a design, refer to a design by its name (LABEL1_vs_LABEL2)
+    # Choosing balance correction value : see documentation for explanation
+    mtb_ctrl.set_balance_correction_for_experiment("Cases_vs_Ctrls", 0)
+    #mtb_ctrl.set_balance_correction_for_experiment("NEG_Case_Current_vs_Ctrl_Current", 15)
+    #mtb_ctrl.set_balance_correction_for_experiment("NEG_Case_Former_vs_Ctrl_Former", 0)
+    #mtb_ctrl.set_balance_correction_for_experiment("NEG_Case_Never_vs_Ctrl_Never", 21)
+    mtb_ctrl.create_splits()
+
+
+def ML_setup_CV_and_algo(mtb_ctrl, cv_algo):
+    """
+    mtb_ctrl : MetaboController object
+    cv_algo : Either GridSearchCV(default) or RandomizedSearchCV
+    """
+    mtb_ctrl.set_multithreading(True)
+
+    # Available defaults : ["DecisionTree", "RandomForest", "SCM", "RandomSCM"]
+    mtb_ctrl.set_selected_models(["DecisionTree", "RandomForest", "SCM", "RandomSCM"])
+    
+    # (if GridSearch you can simply comment the line)
+    mtb_ctrl.set_cv_type(cv_algo)
+    
+    # Needed if RandomizedSearchCV is chosen
+    # list of values for required parameters of CV algorithm, randomSearch requires n_iter arg : the default here is 10
+    mtb_ctrl.set_cv_algorithm_configuration([8])
+    
+    mtb_ctrl.set_cv_folds(5)
+    mtb_ctrl.learn()
+
+
+def SAVE_setups_and_results(mtb_ctrl, experiment_path):
+    """
+    mtb_ctrl : MetaboController object
+    experiment_path : either 
+                    -   'medic_splits' the save of only splits parameters, after the "splits tab"
+                    -   'medic_ml' the save of all parameters and results, after the "ml tab"
+    """
+    metabo_expe_filename = Utils.get_metabo_experiment_path(experiment_path) # Get save file path
+    metabo_expe_obj = mtb_ctrl.generate_save()
+    Utils.dump_metabo_expe(metabo_expe_obj) # Dump the classification design to the dump folder
+    Utils.dump_metabo_expe(metabo_expe_obj, metabo_expe_filename) # Save the classification design
+    # free memory from object
+    del metabo_expe_obj
+
+
+###############################
+#        Main function        #
+###############################
+
+
 def main():
     start_time = datetime.now()
     print("Starting MeDIC at : ", start_time)
     metabo_controller = MetaboController()
-    metabo_controller.set_raw_use_for_data(False)
-    metabo_controller.set_data_matrix_remove_rt(False)
-    metabo_controller.set_multithreading(True)
 
-    metabo_controller.set_data_matrix_from_path(DATAMATRIX_PATH, from_base64=False)
-    metabo_controller.set_metadata(METADATA_PATH, from_base64=False)
-    print("Metadata and DataMatrix are set_from_path")
-
-    metabo_controller.set_id_column("Sample_Name")
-    metabo_controller.set_target_columns(["Factor Value[Sample Type]"])
-    metabo_controller.add_experimental_design({"Cases": ["Case"], "Ctrls": ["Control"]})
-    #metabo_controller.set_target_columns(["Factor Value[Sample Type]", "Factor Value[Smoking]"])
-    #metabo_controller.add_experimental_design({"NEG_Case_Current": ["Case__Current Smoker"], "Ctrl_Current": ["Control__Current Smoker"]})
-    #metabo_controller.add_experimental_design({"NEG_Case_Former": ["Case__Former Smoker"], "Ctrl_Former": ["Control__Former Smoker"]})
-    #metabo_controller.add_experimental_design({"NEG_Case_Never": ["Case__Never Smoker"], "Ctrl_Never": ["Control__Never Smoker"]})
-    print("Classification design added")
-
-    metabo_controller.set_train_test_proportion(0.2)
-    metabo_controller.set_number_of_splits(30)
-
-    ### Choosing balance correction value : see documentation for explanation
-    metabo_controller.set_balance_correction_for_experiment("Cases_vs_Ctrls", 0)
-    #metabo_controller.set_balance_correction_for_experiment("NEG_Case_Current_vs_Ctrl_Current", 15)
-    #metabo_controller.set_balance_correction_for_experiment("NEG_Case_Former_vs_Ctrl_Former", 0)
-    #metabo_controller.set_balance_correction_for_experiment("NEG_Case_Never_vs_Ctrl_Never", 21)
-    metabo_controller.create_splits()
-
-    metabo_expe_filename = Utils.get_metabo_experiment_path("medic_splits") # Get save file path
-    metabo_expe_obj = metabo_controller.generate_save()
-    Utils.dump_metabo_expe(metabo_expe_obj) # Dump the classification design to the dump folder
-    Utils.dump_metabo_expe(metabo_expe_obj, metabo_expe_filename) # Save the classification design
-    del metabo_expe_obj
-
-    metabo_controller.set_selected_models(["DecisionTree", "RandomForest", "SCM", "RandomSCM"])
-    print("Learning starts...")
-    metabo_controller.set_cv_type("RandomizedSearchCV") # Otherwise default is GridSearchCV (if you want GridSearch you can either specifiy it here, or simply comment this line)
-    metabo_controller.set_cv_algorithm_configuration([10]) # needed if randomSearch is selected, list of values for required parameters of CV algorithm, randomSearch requires n_iter arg : the default here is 10
-    metabo_controller.set_cv_folds(5)
-    metabo_controller.learn()
-    print("finished")
+    SPLITS_setup_files(metabo_controller)
+    SPLITS_setup_classification_designs(metabo_controller)
+    SPLITS_setup_splits_and_balancing(metabo_controller, 0.2, 5)
+    SAVE_setups_and_results(metabo_controller, "medic_splits")
     
-    metabo_expe_filename = Utils.get_metabo_experiment_path("medic_ml") # Get save file path
-    metabo_expe_obj = metabo_controller.generate_save()
-    Utils.dump_metabo_expe(metabo_expe_obj) # Dump the classification design to the dump folder
-    Utils.dump_metabo_expe(metabo_expe_obj, metabo_expe_filename) # Save the classification design
-    del metabo_expe_obj
-    print("saved in file")
+    ML_setup_CV_and_algo(metabo_controller, "RandomizedSearchCV")
+    SAVE_setups_and_results(metabo_controller, "medic_ml")
 
     end_time = datetime.now()
     print("Duration: {}".format(end_time - start_time))
